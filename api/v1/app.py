62a3cf532413d336886019bcc4898ef4e8476b0b
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-01-09 01:17:10 +1000

    --set-upstream

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+""" Import modules """
+from api.v1.views import app_views
 from flask import Flask
 from models import storage
-from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
 
+""" register blueprint """
 app.register_blueprint(app_views)
 
+""" Teardown appcontext """
 @app.teardown_appcontext
 def tear_down(error):
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,9 @@ from api.v1.views import app_views
 from flask import jsonify
 
 """ create route """
+
+
 @app_views.route('/status', strict_slashes=False)
 def get_status():
     """Return status"""
     return jsonify({ "status": "OK" })
-
