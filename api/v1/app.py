fbfb2d659a5d69aba6707b1a17a5d1bd43ca3011
willyopera <willy4opera@gmail.com>
2024-01-08 18:42:49 -0500

    Formatted App.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def close_db(error):
     """ Close Storage """
